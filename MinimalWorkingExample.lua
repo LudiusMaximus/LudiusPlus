b3a3d237eab1a3ed3f5332359d143649f5ce3ee2
LudiusMaximus <4678285+LudiusMaximus@users.noreply.github.com>
2025-01-27 17:57:44 +0100

    Added PrintTable() for debugging.

--- MinimalWorkingExample.lua
+++ MinimalWorkingExample.lua
@@ -11,6 +11,37 @@
 
 
 
+-- -- For debugging.
+-- function PrintTable(t, indent)
+  -- assert(type(t) == "table", "PrintTable() called for non-table!")
+  
+  -- print("PrintTable")
+  
+  -- if not indent then indent = 0 end
+
+  -- local indentString = ""
+  -- for i = 1, indent do
+    -- indentString = indentString .. "  "
+  -- end
+
+  -- for k, v in pairs(t) do
+    -- if type(v) ~= "table" then
+
+      -- -- if type(v) == "string" and string_find(v, "Steak") then
+      -- if type(v) == "string" then
+        -- print(indentString, k, "=", v)
+      -- end
+    -- else
+      -- print(indentString, k, "=")
+      -- print(indentString, "  {")
+      -- PrintTable(v, indent + 2)
+      -- print(indentString, "  }")
+    -- end
+  -- end
+-- end
+
+
+
 
 
 
